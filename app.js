32af6b44c76fc98a4f124ad77a320ecb6c6a91b3
Jatin solanki <jatinsolanki1202@gmail.com>
2024-08-07 17:19:19 +0530

    update app.js add views directory

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const dotenv = require('dotenv')
 
 dotenv.config();
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "public")));
